41aeae366c84ed22473d371ca7857d62b8d5b558
Om Burande <111966902+Omburande001@users.noreply.github.com>
2026-03-27 15:39:47 +0530

    Update server

--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ const app = express();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -40,11 +42,14 @@ app.get("/api/debug/check", async (req, res) => {
 });
 
 // MongoDB Connection
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("✅ MongoDB Connected to:", mongoose.connection.name);
-        app.listen(process.env.PORT || 5000, () => {
-            console.log("🚀 Server running on port 5000");
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on port ${PORT}`);
         });
     })
     .catch(err => {
